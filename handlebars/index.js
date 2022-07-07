04862728c6cc398961b431989308f43dd8fd3016
andev <apoysegu@gmail.com>
2022-07-07 18:24:40 -0300

    entrega 6

--- handlebars/index.js
+++ handlebars/index.js
@@ -12,31 +12,7 @@ const io = new IOServer(httpServer)
 
 const PORT = 8080;
 
-// app.engine(
-//   "hbs",
-//   handlebars.engine({
-//     extname: ".hbs",
-//     defaultLayout: "main.hbs",
-//   })
-// );
 
-// app.set("view engine", "hbs");
-// app.set("views", "../views");
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use("/productos", productRouter);
-
-// app.get("/", (req, res) => {
-//   res.render("forms");
-// });
-
-
-
-// const server = app.listen(PORT, () => {
-//   console.log(`Escuchando en el puerto ${server.address().port}`)
-// })
-// server.on("error", error => console.log(`Error en el servidor. ${error}`))
 const messageApi = new ContenedorMemoria()
 const ProductsApi= new FilesystemContainer()
 
